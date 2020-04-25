1d22d7cb72f818257543df9dd2a296b731086af3
umair <umair@datavis.sg>
2020-04-25 13:53:21 +0500

    simple harpgl map display

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
 import StaticMap from 'react-map-gl';
-import {MapboxLayer} from '@deck.gl/mapbox';
+import { MapView, MapViewEventNames } from "@here/harp-mapview";
+import { GeoCoordinates } from "@here/harp-geoutils";
+import { APIFormat, AuthenticationMethod, AuthenticationTypeMapboxV4, OmvDataSource } from "@here/harp-omv-datasource";
 const MAPBOX_ACCESS_TOKEN  = 'pk.eyJ1IjoiZ2FsbGlja2d1bm5lciIsImEiOiJjanliaWs1MGwwM2o1M2dxYTA4cjNscXR4In0.NRBmABrmd87kEEVaYGvxqg';
 
 class Root extends React.Component{
@@ -21,11 +23,30 @@ class Root extends React.Component{
       },
       gl: {},
     };
-  }
-  
-  onMapLoad = () => {
-    this.map.addLayer(new MapboxLayer({ id: 'outline', deck: this.deck}));
-    this.forceUpdate();
+
+    this.mapCanvas = document.getElementById("mapCanvas");
+    this.mapView = new MapView({
+      canvas: mapCanvas,
+      theme: "https://assets.vector.hereapi.com/styles/berlin/base/harp.gl/tilezen?apikey=YZXUgJpknqSz7OH05fKRJBz6k9lKFRe4m5KYtNMjPxc",      
+      decoderUrl: "./harpgl-decoder.bundle.js"
+    });
+    this.mapView.camera.position.set(0, 0, 800);
+    this.mapView.geoCenter = new GeoCoordinates(40.70398928, -74.01319808, 0);
+    this.mapView.resize(this.mapCanvas.clientWidth, this.mapCanvas.clientHeight);
+
+    const dataSource = new OmvDataSource({
+      baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+      apiFormat: APIFormat.XYZOMV,
+      styleSetName: "tilezen",
+      authenticationCode: "YZXUgJpknqSz7OH05fKRJBz6k9lKFRe4m5KYtNMjPxc",
+      authenticationMethod: {
+                method: AuthenticationMethod.QueryString,
+                name: "apkey"
+            },
+    });
+    this.mapView.addDataSource(dataSource);
+    window.onresize = () => this.mapView.resize(window.innerWidth, window.innerHeight);
+    //this.mapView.addEventListener(MapViewEventNames.Resize, () => { this.mapView.resize(this.mapCanvas.clientWidth, this.mapCanvas.clientHeight) })
   }
 
   onWebGLInitialized = (gl) => {
@@ -39,28 +60,20 @@ class Root extends React.Component{
   render() {
     const {gl} = this.state;
     let layers = [];
-    return (
+    console.log(this.mapView);
+    return (<div/>);
+   /* return (
         <div>
             <DeckGL
-              ref = {ref => { this.deck = ref && ref.deck}}
+              //ref = {ref => { this.deck = ref && ref.deck}}
               viewState={this.state.viewState} 
               layers= {layers}
               controller = {true}
               onViewStateChange = {this.onViewStateChange}
-              onWebGLInitialized = {this.onWebGLInitialized}
-              >
-            { gl && (
-              <StaticMap
-                ref = {ref => { this.map = ref && ref.getMap()}}
-                gl = {gl}
-                mapStyle="mapbox://styles/mapbox/dark-v9"
-                mapboxApiAccessToken= {MAPBOX_ACCESS_TOKEN}
-                onLoad = {this.onMapLoad}
+              //onWebGLInitialized = {this.onWebGLInitialized}
               />
-            )}
-            </DeckGL>
         </div>
-    );
+    );*/
   }
 }
 export default Root;
--- src/app.js
+++ src/app.js
@@ -4,3 +4,36 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 
 ReactDOM.render( <Root />, document.getElementById( 'root' ) );
+/*
+import { MapView } from "@here/harp-mapview";
+import { GeoCoordinates } from "@here/harp-geoutils";
+import { APIFormat, AuthenticationMethod, AuthenticationTypeMapboxV4, OmvDataSource } from "@here/harp-omv-datasource";
+
+const mapCanvas = document.getElementById("mapCanvas");
+const mapView = new MapView({
+  canvas: mapCanvas,
+  theme: "https://assets.vector.hereapi.com/styles/berlin/base/harp.gl/tilezen?apikey=YZXUgJpknqSz7OH05fKRJBz6k9lKFRe4m5KYtNMjPxc",
+  // note, this URL may vary depending on configuration of webpack
+  // for this example, it is assumed that app is server from project root
+  //maxVisibleDataSourceTiles: 40, 
+  //tileCacheSize: 100,
+  decoderUrl: "./harpgl-decoder.bundle.js"
+  // note, this URL may vary depending on configuration of webpack
+  // for this example, it is assumed that webpack emits bundles to project root
+});
+
+const dataSource = new OmvDataSource({
+  baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+  apiFormat: APIFormat.XYZOMV,
+  styleSetName: "tilezen",
+  authenticationCode: "YZXUgJpknqSz7OH05fKRJBz6k9lKFRe4m5KYtNMjPxc",
+  authenticationMethod: {
+            method: AuthenticationMethod.QueryString,
+            name: "apkey"
+        },
+});
+mapView.addDataSource(dataSource);
+
+mapView.camera.position.set(0, 0, 800);
+mapView.geoCenter = new GeoCoordinates(40.70398928, -74.01319808, 0);
+mapView.resize(mapCanvas.clientWidth, mapCanvas.clientHeight);*/
