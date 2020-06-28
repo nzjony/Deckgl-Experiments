5a96eeacc5ab3411c313e88a9a4e457e258ea7a5
gallickgunner <ahmed.u@outlook.com>
2020-06-28 05:41:44 -0700

    updated root

--- src/Root.js
+++ src/Root.js
@@ -45,127 +45,7 @@ class Root extends React.Component{
       this.dataArr = data;
       this.forceUpdate();
     });
-  }
-
-
-  resizeFBO = () => {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    const gl = this.webglContext;
-
-    /* global window */
-    const dpr = window.devicePixelRatio;
-    width = Math.round(width * dpr);
-    height = Math.round(height * dpr);
-
-    this.deckFbo.resize({width, height});
-  }
-
-  onLoad = () => {
-    console.log('here');
-    const gl = this.state.gl;
-    instrumentGLContext(gl);
-  
-    this.buffer = new Buffer(gl, new Int8Array([-1, -1, 1, -1, -1, 1, 1, 1]));
-  
-    this.model = new Model(gl, {
-      vs: `
-        attribute vec2 a_pos;
-        varying vec2 v_texcoord;
-        void main(void) {
-            gl_Position = vec4(a_pos, 0.0, 1.0);
-            v_texcoord = (a_pos + 1.0) / 2.0;
-        }
-      `,
-      fs: `
-        precision mediump float;
-        uniform sampler2D u_texture;
-        varying vec2 v_texcoord;
-        void main(void) {
-            vec4 rgba = texture2D(u_texture, v_texcoord);
-            rgba.rgb *= rgba.a;
-            gl_FragColor = rgba;
-        }
-      `,
-      attributes: {
-        a_pos: this.buffer
-      },
-      vertexCount: 4,
-      drawMode: gl.TRIANGLE_STRIP
-    }); 
-
-    this.deckFbo = new Framebuffer(gl, {width: window.innerWidth, height: window.innerHeight});
-    this.deck.setProps({ _framebuffer: this.deckFbo });
-
-  }
-
-  afterRender = ({gl}) => {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    const screenFbo = gl.getParameter(gl.FRAMEBUFFER_BINDING);
-
-    /* global window */
-    const dpr = window.devicePixelRatio;
-    width = Math.round(width * dpr);
-    height = Math.round(height * dpr);
-
-    withParameters(
-      gl,
-      {
-        blend: true,
-        blendFunc: [gl.ONE, gl.ONE_MINUS_SRC_ALPHA],
-        framebuffer: screenFbo,
-        viewport: [0, 0, width, height]
-      },
-      () => {
-        this.model.setUniforms({u_texture: this.deckFbo}).draw();
-      }
-    );
-  }
-
-  initializeResources = () => {    
-    this.deck = new Deck({
-
-      style: {zIndex: 0},
-      // The view state will be set dynamically to track the MapView current extent.
-      viewState: this.state.viewState,
-
-      onViewStateChange: this.onViewStateChange,
-  
-      // Input is handled by the ArcGIS API for JavaScript.
-      controller: true,
-      
-      onBeforeRender: (gl) => { this.resizeFBO() },
-
-      onAfterRender: this.afterRender,
-      onWebGLInitialized: this.onWebGLInitialized,
-
-      onLoad: this.onLoad,
-      // We use the same WebGL context as the ArcGIS API for JavaScript.
-      //gl,
-  
-      // We need depth testing in general; we don't know what layers might be added to the deck.
-      parameters: {
-        depthTest: true
-      },
-  
-      // This deck renders into an auxiliary framebuffer.
-      _framebuffer: this.deckFbo,
-  
-      /*_customRender: redrawReason => {
-        if ( redrawReason === 'basemap redrawn' ) {
-        // base map just redrew, due to e.g. tiles being loaded
-        // render all layers without clearing the canvas
-          this.deck._drawLayers( redrawReason, {clearCanvas: false});
-        } else {
-          // Triggered by other reasons - layer update, view state change, transition, etc.
-          // Tell the base map to redraw first
-          //this.mapView.update();
-          //this.deck._drawLayers( redrawReason, { clearCanvas: false } );
-        }
-      },*/
-    });
-  }
+  }  
 
   initMapView = () => {
     const mapCanvas = document.getElementById( 'mapCanvas' );
@@ -225,7 +105,7 @@ class Root extends React.Component{
     console.log("deck-load");
     this.deck.setProps( {
 
-      parameters: {framebuffer: null},
+      //parameters: {framebuffer: null},
 
       // Normally, when deck renders to the canvas, it will wipe the existing content and draw all layers.
       // This callback overrides the default behavior      
