5b901316a7f05b46365fac5644a4d30c0bb94250
gallickgunner <ahmed.u@outlook.com>
2020-07-29 11:50:29 +0500

    debugging depth issue. Inconsistency in near/far plane values between harp and deck.

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import {ArcLayer} from '@deck.gl/layers';
-import { MapView, MapViewEventNames, MapViewUtils } from "@here/harp-mapview";
+import {ArcLayer, IconLayer} from '@deck.gl/layers';
+import { MapView, MapViewEventNames, MapViewUtils, FixedClipPlanesEvaluator, TopViewClipPlanesEvaluator, InterpolatedClipPlanesEvaluator, TiltViewClipPlanesEvaluator } from "@here/harp-mapview";
 import { GeoCoordinates } from "@here/harp-geoutils";
 import { APIFormat, AuthenticationMethod, AuthenticationTypeMapboxV4, OmvDataSource } from "@here/harp-omv-datasource";
 import loadCSV from './DataLoader';
 import {Model, Buffer, Framebuffer, instrumentGLContext, withParameters} from '@luma.gl/core';
 import {Deck} from '@deck.gl/core';
+import { MapControls } from "@here/harp-map-controls";
 
 class Root extends React.Component{
 
@@ -16,20 +17,24 @@ class Root extends React.Component{
     this.deck = null;
     this.state = {          
       viewState: {
-        latitude: 41.894683169437165,
-        longitude: 12.507511269849497,
-        bearing: 82,
-        pitch: 54,
-        zoom: 17,
+        latitude:  1.3778398134133185,
+        longitude: 103.8857512606223,
+        bearing: -152.9661117325789,
+        pitch: 55.768917935319195,
+        zoom:  16.76407108308305,
         maxZoom: 21,
       },
+      near: 0.1,
+      far: 3,
       gl: {},
     };  
-    this.initMapView(); 
+   this.initMapView(); 
   }
 
   onWebGLInitialized = (gl) => {
-    console.log("gl-init");    
+    console.log("gl-init");
+    //For testing of passing deck's context to harp
+    //this.initMapView(gl);    
     this.setState({gl});
   }
 
@@ -37,18 +42,32 @@ class Root extends React.Component{
     const coords = new GeoCoordinates( viewState.latitude, viewState.longitude );
     this.mapView.lookAt( coords, MapViewUtils.calculateDistanceFromZoomLevel( { focalLength: this.mapView.focalLength }, viewState.zoom + 1 ), viewState.pitch, viewState.bearing );
     this.mapView.zoomLevel = viewState.zoom + 1;
-    this.setState( { viewState } );
+    const viewport = this.deck.getViewports()[0];
+    const height = viewport.height;
+    let {near, far} = viewport.projectionProps;
+    
+    //far = far * height / 2 * viewport.zoom / viewport.metersPerPixel;
+
+    //Pass updated near/far values from deck to harp.   
+    this.mapView.updateCameras({
+      near: near,
+      far: far,
+      minimum: 0.1,
+      maximum: 100000,
+    });
+    this.setState( { viewState, near, far} );
   }
 
   componentDidMount = () => {
-    loadCSV('Mustang-Arc-2.csv', 'arc', true).then(data => {
+    loadCSV('icon.csv', 'icon', true).then(data => {
+     // loadCSV('arc.csv', 'arc', true).then(data => {
       this.dataArr = data;
       this.forceUpdate();
     });
   }  
 
-  initMapView = () => {
-    const mapCanvas = document.getElementById( 'mapCanvas' );
+  initMapView = (gl) => {
+    const mapCanvas = document.getElementById( 'mapCanvas' );    
     const contextAttributes = {
 			alpha: false,
 			depth: true,
@@ -66,25 +85,30 @@ class Root extends React.Component{
     const viewState = this.state.viewState;
     this.mapView = new MapView( {
       canvas: mapCanvas,
-      //theme: "/resources/berlin_tilezen_day_reduced.json",
-      //theme: 'https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json',
+      //context: gl,
+
+      //Throws error if intially far value set to low values like 3. If set to high values 1000+ initially and changed to low values later through `updateCameras` error doesn't occur.
+      clipPlanesEvaluator: new FixedClipPlanesEvaluator(0.1, 3),
+
+      //clipPlanesEvaluator: new InterpolatedClipPlanesEvaluator(0.1, 0.1, 35, 300),
       theme: 'https://assets.vector.hereapi.com/styles/berlin/base/harp.gl/tilezen?apikey=YZXUgJpknqSz7OH05fKRJBz6k9lKFRe4m5KYtNMjPxc',
       zoomLevel: viewState.zoom,
       decoderUrl: './harpgl-decoder.bundle.js',
-      enableNativeWebglAntialias: true,      
     } );
-    
+
+    //For testing without deckgl
+    //MapControls.create(this.mapView);
+
     this.mapView.addEventListener( MapViewEventNames.AfterRender, () => {
       if ( this.deck ) { this.deck.redraw( 'basemap redrawn', true ); }
     } );
 
     const coords = new GeoCoordinates( viewState.latitude, viewState.longitude );
-    this.mapView.setFovCalculation( { fov: 36, type: 'fixed' } );
+    this.mapView.setFovCalculation( { fov: 37, type: 'fixed' } );
     this.mapView.maxZoomLevel = 21;
     this.mapView.geoCenter = coords;
     this.mapView.lookAt( coords, MapViewUtils.calculateDistanceFromZoomLevel( { focalLength: this.mapView.focalLength }, viewState.zoom + 1 ), viewState.pitch, viewState.bearing );
-    this.mapView.zoomLevel = viewState.zoom + 1;
-
+    this.mapView.zoomLevel = viewState.zoom + 1;    
     this.mapView.resize( mapCanvas.clientWidth, mapCanvas.clientHeight );
 
     const dataSource = new OmvDataSource( {
@@ -103,9 +127,12 @@ class Root extends React.Component{
 
   onDeckGlLoad = () => {
     console.log("deck-load");
+    const viewport = this.deck.getViewports()[0];
+    const {near, far} = viewport.projectionProps;
+
     this.deck.setProps( {
 
-      //parameters: {framebuffer: null},
+      parameters: {framebuffer: null},
 
       // Normally, when deck renders to the canvas, it will wipe the existing content and draw all layers.
       // This callback overrides the default behavior      
@@ -122,32 +149,37 @@ class Root extends React.Component{
       }
       },
     } );
+    this.setState( { near, far } );
   }
 
   render() {
-    let layers = [];
-    layers.push( new ArcLayer({
-      id: 'arc2',
+    let layers = [];   
+    layers.push( new IconLayer( {
+      id: 'icon-layer',
       data: this.dataArr? this.dataArr: null,
       pickable: true,
       autoHighlight: true,
-      getSourcePosition: d => [ d.srcLon, d.srcLat ],
-      getTargetPosition: d => [ d.targetLon, d.targetLat ],
-      getSourceColor: [180,0,0,255],
-      getTargetColor: [180,0,0,255],
-      widthScale: 3.0
+      billboard: true,
+      getIcon: d => ( {
+          url: d.icon,
+          width: 300,
+          height: 300,
+          mask: false,
+        } ),
+      getSize: d => ( 35 + Math.floor( 35 * Math.random() * 0.65 ) ),
+      getPosition: d => [ d.lon, d.lat ]
     }));
+
     return (
       <div>
         <DeckGL
-          style = {{zIndex: 0}}
           ref = {ref => { this.deck = ref && ref.deck}}
           viewState={this.state.viewState} 
           layers= {layers}
           controller = {true}
           onViewStateChange = {this.onViewStateChange}
           onLoad={ this.onDeckGlLoad }
-          //onWebGLInitialized = {this.onWebGLInit}
+          //onWebGLInitialized = {this.onWebGLInitialized}
           gl={ this.webglContext }
           parameters = {{
             depthTest: true
